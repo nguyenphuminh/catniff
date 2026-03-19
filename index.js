9a52c93ef08152054d6bd381884ed69056320059
Phu Minh (Catto) <53084840+nguyenphuminh@users.noreply.github.com>
2026-03-19 13:56:03 +0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    ...require("./dist/core"),
-    ...require("./dist/optim"),
-    ...require("./dist/nn"),
-    ...require("./dist/lrscheduler"),
-    ...require("./dist/dtype")
-};
